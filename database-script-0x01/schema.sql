cc1b80bad31ef46ff740609bb25f37292dfacd8f
Victor Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2025-05-11 00:15:19 +0300

    Property table DDL

--- database-script-0x01/schema.sql
+++ database-script-0x01/schema.sql
@@ -9,3 +9,15 @@ role CHECK (role IN('guest','host','admin')) NOT NULL,
 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE TABLE Property(
+property_id UUID Primary Key,
+  host_id UUID,
+name VARCHAR NOT NULL,
+description TEXT NOT NULL,
+  location VARCHAR NOT NULL,
+pricepernight DECIMAL NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (host_id) REFERENCES "User"(user_id)
+);
+
