31f93802d61f7d666bcf870cac80d2b5e04125e3
Victor Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2025-05-10 23:45:15 +0300

    User Table DDL

--- database-script-0x01/schema.sql
+++ database-script-0x01/schema.sql
@@ -1,1 +1,11 @@
+CREATE TABLE User(
+user_id UUID PRIMARY KEY,
+first_name VARCHAR NOT NULL,
+last_name VARCHAR NOT NULL,
+email VARCHAR UNIQUE NOT NULL,
+password_hash VARCHAR NOT NULL,
+phone_number VARCHAR NULL,
+role CHECK (role IN('guest','host','admin')) NOT NULL,
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
