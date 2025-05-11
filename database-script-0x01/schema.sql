a1876ee0b9e258a7222aa2cb62184fe2c9b757c0
Victor Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2025-05-11 18:53:58 +0300

    Update schema.sql

--- database-script-0x01/schema.sql
+++ database-script-0x01/schema.sql
@@ -5,7 +5,7 @@ last_name VARCHAR NOT NULL,
 email VARCHAR UNIQUE NOT NULL,
 password_hash VARCHAR NOT NULL,
 phone_number VARCHAR NULL,
-role CHECK (role IN('guest','host','admin')) NOT NULL,
+role VARCHAR NOT NULL CHECK (role IN('guest','host','admin')),
 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
@@ -18,7 +18,7 @@ description TEXT NOT NULL,
 pricepernight DECIMAL NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (host_id) REFERENCES "User"(user_id)
+  FOREIGN KEY (host_id) REFERENCES User(user_id)
 );
 
 CREATE TABLE Booking(
@@ -28,9 +28,17 @@ user_id UUID,
 start_date DATE NOT NULL,
 end_date DATE NOT NULL,
 total_price DECIMAL NOT NULL,
-status CHECK status in ('pending', 'confirmed', 'canceled') NOT NULL,
+status VARCHAR NOT NULL CHECK status in ('pending', 'confirmed', 'canceled')L,
 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-FOREIGN KEY REFERENCES User(user_id),
-FOREIGN KEY REFERENCES Property(property_id)
+FOREIGN KEY(user_id) REFERENCES User(user_id),
+FOREIGN KEY(property_id) REFERENCES Property(property_id)
 );
 
+CREATE TABLE Payment(
+payment_id UUID Primary Key,
+booking_id UUID NOT NULL,
+amount DECIMAL(18,2) NOT NULL,
+payment_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+payment_method VARCHAR NOT NULL CHECK (payment_method in ('credit_card','paypal', 'stripe')),
+    FOREIGN KEY (booking_id) REFERENCES Booking(booking_id)
+)
