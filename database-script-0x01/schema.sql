8f74ae06b76a2c50b439910edafd6071d9597e3b
Victor Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2025-05-13 14:24:47 +0300

    Update schema.sql

--- database-script-0x01/schema.sql
+++ database-script-0x01/schema.sql
@@ -1,5 +1,7 @@
-CREATE TABLE User(
-user_id UUID PRIMARY KEY,
+-- Enable pgcrypto extension (only once per database)
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+CREATE TABLE "User"(
+user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
 first_name VARCHAR NOT NULL,
 last_name VARCHAR NOT NULL,
 email VARCHAR UNIQUE NOT NULL,
@@ -10,7 +12,7 @@ created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE Property(
-property_id UUID Primary Key,
+property_id UUID Primary Key DEFAULT gen_random_uuid(),
   host_id UUID,
 name VARCHAR NOT NULL,
 description TEXT NOT NULL,
@@ -18,23 +20,23 @@ description TEXT NOT NULL,
 pricepernight DECIMAL NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (host_id) REFERENCES User(user_id)
+  FOREIGN KEY (host_id) REFERENCES "User"(user_id)
 );
 
-CREATE TABLE Booking(
+CREATE TABLE Booking DEFAULT gen_random_uuid()(
 booking_id UUID Primary Key,
 property_id UUID,
 user_id UUID,
 start_date DATE NOT NULL,
 end_date DATE NOT NULL,
 total_price DECIMAL NOT NULL,
-status VARCHAR NOT NULL CHECK status in ('pending', 'confirmed', 'canceled')L,
+status VARCHAR NOT NULL CHECK (status in ('pending', 'confirmed', 'canceled')),
 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-FOREIGN KEY(user_id) REFERENCES User(user_id),
+FOREIGN KEY(user_id) REFERENCES "User"(user_id),
 FOREIGN KEY(property_id) REFERENCES Property(property_id)
 );
 
-CREATE TABLE Payment(
+CREATE TABLE Payment DEFAULT gen_random_uuid()(
 payment_id UUID Primary Key,
 booking_id UUID NOT NULL,
 amount DECIMAL(18,2) NOT NULL,
@@ -44,11 +46,11 @@ payment_method VARCHAR NOT NULL CHECK (payment_method in ('credit_card','paypal'
 )
 
 CREATE TABLE Message(
-message_id UUID Primary Key,
+message_id UUID Primary Key DEFAULT gen_random_uuid(),
 sender_id,
 recipient_id,
 message_body TEXT NOT NULL,
 sent_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-FOREIGN KEY(sender_id) REFERENCES User(user_id),
-FOREIGN KEY(recipient_id) REFERENCES User(user_id)
+FOREIGN KEY(sender_id) REFERENCES "User"(user_id),
+FOREIGN KEY(recipient_id) REFERENCES "User"(user_id)
 )
