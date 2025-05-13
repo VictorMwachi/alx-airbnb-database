99f9d331d350e2ed1b782c1afd56631ab0870229
Victor Mwachi <mudakizevictor@gmail.com>
2025-05-13 11:48:38 +0300

    correlated subquery

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,1 +1,2 @@
-SELECT * FROM Properties WHERE property_id in (SELECT property_id FROM Reviews GROUP BY property_id having AVG(rating)>4);
+SELECT * FROM Properties WHERE property_id in (SELECT property_id FROM Reviews GROUP BY property_id HAVING AVG(rating)>4.0);
+SELECT * FROM User WHERE user_id IN (SELECT user_id FROM Booking GROUP BY user_id HAVING COUNT(*)>3)
