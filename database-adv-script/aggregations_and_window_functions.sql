9836d4687f257964e4494af65e0d13c95fddb582
Victor Mwachi <mudakizevictor@gmail.com>
2025-05-18 17:20:31 +0300

    rank function

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -12,3 +12,17 @@ FROM (
     GROUP BY 
         property_id
 ) AS property_bookings;
+
+SELECT 
+    property_id,
+    total_bookings,
+    RANK() OVER (ORDER BY total_bookings DESC) AS booking_rank
+FROM (
+    SELECT 
+        property_id,
+        COUNT(*) AS total_bookings
+    FROM 
+        bookings
+    GROUP BY 
+        property_id
+) AS property_bookings;
