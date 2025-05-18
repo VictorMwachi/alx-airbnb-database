9d368319f6c8a67554a21c29853b57af285f2270
Victor Mwachi <mudakizevictor@gmail.com>
2025-05-18 20:11:46 +0300

    update

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -5,3 +5,12 @@
         Property prty ON book.property_id = prty.property_id 
     INNER JOIN 
         Payment pmnt ON book.booking_id = pmnt.booking_id
+
+EXPLAIN ANALYZE
+SELECT * FROM Booking book 
+    INNER JOIN 
+        User guest ON book.user_id = guest.user_id 
+    INNER JOIN 
+        Property prty ON book.property_id = prty.property_id 
+    INNER JOIN 
+        Payment pmnt ON book.booking_id = pmnt.booking_id;
